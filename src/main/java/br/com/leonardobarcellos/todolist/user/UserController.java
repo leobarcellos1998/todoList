f95fc1a6be8d69b248286b2eefbb358913abce11
Leonardo Barcellos <permitent.siqueira@gmail.com>
2023-10-10 16:04:55 -0300

    Projeto todolist AULA 2

--- src/main/java/br/com/leonardobarcellos/todolist/user/UserController.java
+++ src/main/java/br/com/leonardobarcellos/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.leonardobarcellos.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,18 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    /*
-     * String (Texto)
-     * integer (Int)
-     * Double Números com virgula
-     * Float Números com virgulas 0.000
-     * char um caractere
-     * Date datas
-     * void
-     */
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+           
+            //MSG Erro
+            //Status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
     
